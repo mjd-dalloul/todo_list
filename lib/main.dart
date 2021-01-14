073acf470751678b5e57b7bcacef13b6d0964bd1
mjd <majed.dalloul79@gmail.com>
2021-01-14 18:17:30 +0200

    setup firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_list/core/firbase_connection/bloc/firebase_connection_bloc.dart';
 
-void main() =>runApp(MyApp());
-
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,15 +13,29 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: BlocProvider<FirebaseConnectionBloc>(
+          create: (_) => FirebaseConnectionBloc()..add(StartConnecting()),  //todo dependency injection
+          child: Root()
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-
+class Root extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: BlocBuilder<FirebaseConnectionBloc, FirebaseConnectionState>(
+        builder: (context, state) {
+          if(state is ConnectingToFirebase) {
+            return Center(child: CircularProgressIndicator(),);
+          } else if(state is ConnectionDone) {
+            return Center(child: Text('DONE'),);
+          } else if(state is ConnectionFailed) {
+            return Center(child: Text('${state.massage}'),);
+          } else return Container();
+        },
+      ),
+    );
   }
 }
