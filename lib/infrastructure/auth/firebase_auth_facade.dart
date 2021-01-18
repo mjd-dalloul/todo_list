c31dc8e23c5c70659e4a91f36d4520d5766c2542
mjd <majed.dalloul79@gmail.com>
2021-01-19 00:59:55 +0200

    implement IAuthFacade 1.2

--- lib/infrastructure/auth/firebase_auth_facade.dart
+++ lib/infrastructure/auth/firebase_auth_facade.dart
@@ -9,31 +9,37 @@ import 'package:todo_list/domain/auth/value_objects.dart';
 class FirebaseAuthFacade implements IAuthFacade {
   final FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
+
   FirebaseAuthFacade(this._firebaseAuth, this._googleSignIn);
+
   @override
-  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({EmailAddress emailAddress, Password password}) async {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
+      {EmailAddress emailAddress, Password password}) async {
     final email = emailAddress.getValue();
     final passwordStr = password.getValue();
-    try{
-      await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: passwordStr);
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: passwordStr);
       return Right(unit);
     } on FirebaseAuthException catch (e) {
-      if(e.code == 'ERROR_EMAIL_ALREADY_IN_USE') {
+      if (e.code == 'ERROR_EMAIL_ALREADY_IN_USE') {
         return Left(const AuthFailure.emailAlreadyInUse());
-      } else return Left(const AuthFailure.serverError());
+      } else
+        return Left(const AuthFailure.serverError());
     }
   }
 
   @override
-  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({EmailAddress emailAddress, Password password}) async {
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword(
+      {EmailAddress emailAddress, Password password}) async {
     final email = emailAddress.getValue();
     final passwordStr = password.getValue();
-    try{
-      await _firebaseAuth.signInWithEmailAndPassword(email: email, password: passwordStr);
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: passwordStr);
       return Right(unit);
     } on PlatformException catch (e) {
-      if(e.code == 'INVALID_EMAIL' ||
-         e.code == 'WRONG_PASSWORD') {
+      if (e.code == 'INVALID_EMAIL' || e.code == 'WRONG_PASSWORD') {
         return Left(const AuthFailure.invalidEmailAndPassword());
       } else
         return Left(const AuthFailure.serverError());
@@ -41,9 +47,20 @@ class FirebaseAuthFacade implements IAuthFacade {
   }
 
   @override
-  Future<Either<AuthFailure, Unit>> signInWithGoogle() {
-    // TODO: implement signInWithGoogle
-    throw UnimplementedError();
+  Future<Either<AuthFailure, Unit>> signInWithGoogle() async {
+    try {
+      final googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        return Left(const AuthFailure.cancelledByUser());
+      }
+      final googleAuthentication = await googleUser.authentication;
+      final authCredential = GoogleAuthProvider.credential(
+          idToken: googleAuthentication.idToken,
+          accessToken: googleAuthentication.accessToken);
+      await _firebaseAuth.signInWithCredential(authCredential);
+    } on PlatformException {
+      return Left(const AuthFailure.serverError());
+    }
+    return Right(unit);
   }
-
 }
