9bf00e1fb6bc747a27c8cbc968d1c0a41cc36435
mjd <majed.dalloul79@gmail.com>
2021-02-04 03:31:02 +0200

    fixing sign in form

--- lib/presentation/auth/widgets/sign_in_form.dart
+++ lib/presentation/auth/widgets/sign_in_form.dart
@@ -197,6 +197,14 @@ class SignInForm extends StatelessWidget {
                     ),
                   ),
                 ),
+                if (state.isSubmitting) ...[
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  const Center(
+                    child: const CircularProgressIndicator(),
+                  )
+                ]
               ],
             ),
           ),
